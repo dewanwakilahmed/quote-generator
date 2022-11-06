36a96ad3f68fcf1647920c05061e22b6799405a1
Dewan Wakil Ahmed <dewanwakilahmed@gmail.com>
2022-11-06 19:31:53 +0600

    Fetching Data With JavaScript -- Final

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-// let apiQuotes = [];
+let apiQuotes = [];
 
 // Show New Quote
 function newQuote() {
   // Pick a random quote from apiQuotes array
-  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  // const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   console.log(quote);
 }
 
 // Get Quotes From API
-// async function getQuotes() {
-//   const apiUrl = 'https://type.fit/api/quotes';
+async function getQuotes() {
+  const apiUrl = 'https://type.fit/api/quotes';
 
-//   try {
-//     const response = await fetch(apiUrl);
-//     apiQuotes = await response.json();
-//     newQuote();
-//   } catch (error) {
-//     // Catch Error Here
-//   }
-// }
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+  }
+}
 
 // On Load
-// getQuotes();
-newQuote();
+getQuotes();
+// newQuote();
